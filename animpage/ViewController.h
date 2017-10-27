9b914bd8a654939877bed339d934679ea08c346e
Ahmad Saifuddin <saifuddin.fakhri@gmail.com>
2017-10-26 23:42:41 -0500

    first commit

---
+++ animpage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  animpage
+//
+//  Created by Ahmad Saifuddin on 10/26/17.
+//  Copyright © 2017 Ahmad Saifuddin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
